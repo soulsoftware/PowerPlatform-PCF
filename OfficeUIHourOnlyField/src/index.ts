4d44ea334c9a359178b06b8b44d715a4177ff6a2
bsorrentino <bartolomeo.sorrentinogmail.com>
2021-05-10 15:36:04 +0200

    update

--- OfficeUIHourOnlyField/src/index.ts
+++ OfficeUIHourOnlyField/src/index.ts
@@ -64,7 +64,7 @@ export class OfficeUIHourOnlyField implements ComponentFramework.StandardControl
 		this.props = {
 			TimeValue: context.parameters.TimeValue.raw,
 			DefaultDate: context.parameters.DefaultDate.raw,
-			isUTC:(1==DateTimeFieldBehavior),
+			TimeZoneIndependent:(1==DateTimeFieldBehavior),
 			onTimeChange:( value ) => {
 				this.output.TimeValue = value
 				this.notifyOutputChanged()
