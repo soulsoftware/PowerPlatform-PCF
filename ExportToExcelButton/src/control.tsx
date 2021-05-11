865cd9cedde621f71bf89f163d8a6cc91d06e9c1
Build Pipeline <build.pipeline@softphone.it>
2021-05-11 20:34:49 +0200

    catch error

--- ExportToExcelButton/src/control.tsx
+++ ExportToExcelButton/src/control.tsx
@@ -3,6 +3,7 @@ import { IIconProps } from '@fluentui/react/lib/Icon';
 import { ActionButton } from '@fluentui/react/lib/Button';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import * as XLSX from 'xlsx';
+import { ILabelStyles } from 'office-ui-fabric-react/lib/Label';
 
 export interface IControlProps {
     printable?:string
@@ -13,27 +14,41 @@ export interface IControlProps {
 export function initializeControl() {
     initializeIcons()
 }
+
 const excel: IIconProps = { iconName: 'excelDocument' };
+
+const errorStyle:ILabelStyles = {
+    root: {
+        color: 'red'
+    }
+}
 
 export const ExportToExcelControl: React.FunctionComponent<IControlProps> = props => {
   const { printable, filename, text } = props;
 
-
   const isValid = () => printable && filename 
   
-  const exportToExcel = () => {
+  const exportToExcel = React.useCallback(() => {
     
         if( printable && filename ) {
-            const testss = JSON.parse(printable);
-            const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(testss);
-            const workbook: XLSX.WorkBook = {Sheets: {'data': worksheet}, SheetNames: ['data']};
-            XLSX.writeFile(workbook,filename);    
+
+            try {
+                console.log( printable )
+                const testss = JSON.parse(printable);
+                const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(testss);
+                const workbook: XLSX.WorkBook = {Sheets: {'data': worksheet}, SheetNames: ['data']};
+                XLSX.writeFile(workbook,filename);    
+    
+            }
+            catch( e ) {
+                console.error( e )
+            }
         }
 
-  }
+  },[])
 
   return (
-    <ActionButton iconProps={excel} allowDisabledFocus disabled={!isValid()} onClick={exportToExcel} text={text}>
+    <ActionButton iconProps={excel} allowDisabledFocus disabled={!isValid()} onClick={exportToExcel} text={text} >
       Export to Excel
     </ActionButton>
   );
