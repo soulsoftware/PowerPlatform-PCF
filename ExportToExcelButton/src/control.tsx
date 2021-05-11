a51e90b902fa9860f70e59c8ccd77ade888110bf
Build Pipeline <build.pipeline@softphone.it>
2021-05-12 00:09:32 +0200

    update

--- ExportToExcelButton/src/control.tsx
+++ ExportToExcelButton/src/control.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { IIconProps } from '@fluentui/react/lib/Icon';
-import { ActionButton } from '@fluentui/react/lib/Button';
+import { IIconProps } from 'office-ui-fabric-react/lib/Icon';
+import { ActionButton } from 'office-ui-fabric-react/lib/Button';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
-import * as XLSX from 'xlsx';
 import { ILabelStyles } from 'office-ui-fabric-react/lib/Label';
+import * as XLSX from 'xlsx';
 
 export interface IControlProps {
     printable?:string
@@ -26,10 +26,10 @@ const errorStyle:ILabelStyles = {
 export const ExportToExcelControl: React.FunctionComponent<IControlProps> = props => {
   const { printable, filename, text } = props;
 
-  const isValid = () => printable && filename 
-  
+  const isValid = () => printable && filename
+
   const exportToExcel = React.useCallback(() => {
-    
+
         if( printable && filename ) {
 
             try {
@@ -37,8 +37,8 @@ export const ExportToExcelControl: React.FunctionComponent<IControlProps> = prop
                 const testss = JSON.parse(printable);
                 const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(testss);
                 const workbook: XLSX.WorkBook = {Sheets: {'data': worksheet}, SheetNames: ['data']};
-                XLSX.writeFile(workbook,filename);    
-    
+                XLSX.writeFile(workbook,filename);
+
             }
             catch( e ) {
                 console.error( e )
