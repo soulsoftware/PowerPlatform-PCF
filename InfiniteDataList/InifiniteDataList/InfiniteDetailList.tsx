955387f9140e4501e70302da12f40886aa9e778a
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-03-09 19:59:17 +0100

    feat(InfiniteDetailListControl): add new control

---
+++ InfiniteDataList/InifiniteDataList/InfiniteDetailList.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Label } from '@fluentui/react';
+
+export interface InfiniteDetailListProps {
+  name?: string;
+}
+
+const  _InfiniteDetailListControl = ( props: InfiniteDetailListProps ) => {
+  
+    return (
+      <Label>
+        {props.name}
+      </Label>
+    )
+}
+
+export const InfiniteDetailListControl = React.memo( _InfiniteDetailListControl )
